b5cc5cf5e2b562322142f5fe8117977b253e58dd
caperaven <caperaven@gmail.com>
2020-06-12 14:16:55 +0200

    Initial commit

---
+++ app/welcome/welcome.js
@@ -0,0 +1,7 @@
+import {ViewBase} from "./../../node_modules/crs-binding/crs-view-base.js";
+
+export default class Welcome extends ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}

b05d1fde15d900fc1660e343a6cc7900ef9198ec
caperaven <caperaven@gmail.com>
2020-06-12 15:51:34 +0200

    initial

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import {ViewBase} from "./node_modules/crs-binding/crs-view-base.js";
 
 export default class IndexViewModel extends ViewBase {
+    async connectedCallback() {
+        super.connectedCallback();
+        this.routes = this._getRoutes();
+    }
+
+    _getRoutes() {
+        const router = document.querySelector("crs-router");
+
+        const fn = () => {
+            router.removeEventListener("ready", fn);
+            const routes = router.routesDef;
+            console.log(routes);
+        };
+
+        router.addEventListener("ready", fn);
+    }
 }

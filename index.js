6a87c1d9b59223527495aead864e43b59aa9297b
caperaven <caperaven@gmail.com>
2020-07-02 17:03:41 +0200

    initial

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ import {ViewBase} from "./node_modules/crs-binding/crs-view-base.js";
 export default class IndexViewModel extends ViewBase {
     async connectedCallback() {
         await super.connectedCallback();
-        this.routes = this._getRoutes();
+        crsbinding.data.updateUI(this, "routes");
     }
 
-    _getRoutes() {
-        const router = document.querySelector("crs-router");
-        const result = [];
-        const fn = () => {
-            router.removeEventListener("ready", fn);
+    async preLoad() {
+        await this._getRoutes();
+    }
 
-            const routes = router.routesDef;
-            for (let route of routes.routes) {
-                if (route.hash != "#404") {
-                    result.push({title: route.title, hash: route.hash});
+    async _getRoutes() {
+        return new Promise(resolve => {
+            const router = document.querySelector("crs-router");
+            const result = [];
+            const fn = () => {
+                router.removeEventListener("ready", fn);
+
+                const routes = router.routesDef;
+                for (let route of routes.routes) {
+                    if (route.hash != "#404") {
+                        result.push({title: route.title, hash: route.hash});
+                    }
                 }
-            }
-            crsbinding.data.setProperty(this, "routes", result);
-        };
 
-        router.addEventListener("ready", fn);
+                crsbinding.data.setProperty(this, "routes", result);
+                resolve();
+            };
+
+            if (router.routesDef != null) {
+                fn();
+            }
+            else {
+                router.addEventListener("ready", fn);}
+        })
     }
 }

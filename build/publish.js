17a839dab8de8e3dd3037c22411428502d661b73
caperaven <caperaven@gmail.com>
2020-09-19 11:57:43 +0200

    updated publish to publish app files and minify the html

--- build/publish.js
+++ build/publish.js
@@ -2,18 +2,26 @@ const fs = require("fs");
 const mkdirp = require("mkdirp");
 const glob = require("glob");
 const path = require("path");
+const htmlMinifi = require("html-minifier").minify;
 
 class Publish {
     static async distribute() {
         const instance = new Publish();
-        instance.copyFiles("./dist/*.*");
-        instance.copyFiles("./readme.md");
-        //instance.copyFiles("./examples/*.html", "examples");
+        await instance.copyFiles("./dist/*.*");
+        await instance.copyFiles("./readme.md");
+        await instance.copySource("app");
+        await instance.saveCommands();
         instance.bumpVersion();
     }
     
     constructor() {
         mkdirp.sync(path.resolve("./publish"));
+        this.commands = [
+            'terser index.js -c -m -o index.js',
+            'terser node_modules/crs-binding/crs-binding.js -c -m -o node_modules/crs-binding/crs-binding.js',
+            'terser node_modules/crs-binding/crs-bindable-element.js -c -m -o node_modules/crs-binding/crs-bindable-element.js',
+            'terser node_modules/crs-binding/crs-view-base.js -c -m -o node_modules/crs-binding/crs-view-base.js'
+        ];
     }
 
     async copyFiles(query, folder) {
@@ -62,6 +70,44 @@ class Publish {
 
         fs.writeFileSync(sourcePackage, JSON.stringify(pkg, null, 4), "utf8");
         fs.copyFileSync(sourcePackage, targetPackage);
+    }
+
+    async copySource(sourceFolder) {
+        const files = await this.getFiles(`./${sourceFolder}/**/*.*`);
+        for (let file of files) {
+            console.log(file);
+
+            const basePath = path.dirname(file).replace("./", "");
+            const target = `./publish/${basePath}/`;
+            const fileName = path.basename(file);
+            const ext = path.extname(file);
+            this.initFolder(target);
+
+            const toFile = `${target}${fileName}`.replace("publish/", "");
+
+            if (ext == ".js") {
+                this.commands.push(`terser ${toFile} -c -m -o ${toFile}`);
+            }
+
+            if (ext == ".html") {
+                let html = fs.readFileSync(file, "utf8");
+                html = htmlMinifi(html, {
+                    minifyCSS: true,
+                    collapseWhitespace: true
+                });
+                fs.writeFileSync(`${target}${fileName}`, html, "utf8");
+            }
+            else {
+                fs.copyFileSync(file, `${target}${fileName}`);
+            }
+
+        }
+    }
+
+    async saveCommands() {
+        const string = this.commands.join("\n");
+        const target = `./publish/minify.sh`;
+        fs.writeFileSync(target, string, "utf8");
     }
 }
 
